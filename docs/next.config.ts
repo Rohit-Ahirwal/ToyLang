81e6360923edcfd0c581e7ebdc2fb9e33e5397c5
Rohit Ahirwal <rohitraj42087@gmail.com>
2025-01-22 13:27:28 +0530

    fix nextjs config error

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-    }
-  }
 };
 
 export default nextConfig;
